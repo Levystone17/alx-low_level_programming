9417ea41ed97216b453b92e7520b518ca6c07679
Levystone17 <tnlevy91@gmail.com>
2022-09-09 04:03:52 -0700

    task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *
+ *program for printing
+ *
+ */
+
+int man(void)
+{
+char ch = "and that piece of art is useful \"- Dora Korpar, 2015-10-19";
+putchar(ch);
+return (1);
+}
