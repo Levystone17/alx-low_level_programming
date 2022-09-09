ab09eaf2dbd9ae7112dcbc2b9620608ba0609326
Levystone17 <tnlevy91@gmail.com>
2022-09-08 18:25:12 -0700

    task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-char a ;
-int b ;
-long int c ;
-long long int d ;
-float e ;
+char a;
+int b;
+long int c;
+long long int d;
+float e;
 
-printf("size of a char : %c bite(s) \n " , (unsigned long) sizeof(a));
-printf("size of an int : %i bite(s) \n " , (unsigned long) sizeof(b));
-printf("size of a long int : %i bite(s) \n " , (unsigned long) sizeof(c));
-printf("size of a long long int : %i bite(s) \n " , (unsigned long) sizeof(d));
-printf("size of a float : %i bite(s) \n " , (unsigned long ) sizeof(e));
+printf("size of a char : %c bite(s) \n ", (unsigned long) sizeof(a));
+printf("size of an int : %i bite(s) \n ", (unsigned long) sizeof(b));
+printf("size of a long int : %i bite(s) \n ", (unsigned long) sizeof(c));
+printf("size of a long long int : %i bite(s) \n ", (unsigned long) sizeof(d));
+printf("size of a float : %i bite(s) \n ", (unsigned long) sizeof(e));
 return (0);
 }
