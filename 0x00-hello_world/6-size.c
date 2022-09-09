31b5957af1ae4b7340497571f4a2792e47706fdf
Levystone17 <tnlevy91@gmail.com>
2022-09-08 18:50:54 -0700

    task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ long int c;
 long long int d;
 float e;
 
-printf("Size of a char : %i byte(s)\n", (unsigned long) sizeof(a));
-printf("Size of an int : %i byte(s)\n", (unsigned long) sizeof(b));
-printf("Size of a long int : %i byte(s)\n", (unsigned long) sizeof(c));
-printf("Size of a long long int : %i byte(s)\n ", (unsigned long) sizeof(d));
-printf("Size of a float : %i byte(s)\n", (unsigned long) sizeof(e));
+printf("Size of a char: %i byte(s)\n", (unsigned long) sizeof(a));
+printf("Size of an int: %i byte(s)\n", (unsigned long) sizeof(b));
+printf("Size of a long int: %i byte(s)\n", (unsigned long) sizeof(c));
+printf("Size of a long long int: %i byte(s)\n ", (unsigned long) sizeof(d));
+printf("Size of a float: %i byte(s)\n", (unsigned long) sizeof(e));
 return (0);
 }
