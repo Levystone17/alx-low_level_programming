ed5b7890db060cc2357a4bca56ea34e67be7584d
Levystone17 <tnlevy91@gmail.com>
2022-09-11 06:36:58 -0700

    task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints numbers 0-9 without using char or print
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+int i = '0';
+
+while (i <= '9')
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
+}
