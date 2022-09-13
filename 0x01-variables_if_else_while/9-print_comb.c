eddd77b66f539997e440ef9a7ac8c7ac9728681b
Levystone17 <tnlevy91@gmail.com>
2022-09-13 14:05:04 -0700

    task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,16 +8,14 @@
 int main(void)
 
 {
-int i = '0';
-for ((i = '0'); i <= '9'; i++)
+int i;
+for ((i = '0'); i < '9'; i++)
 {
 putchar('i');
-if (i != 9)
-{
 putchar(',');
 putchar(' ');
 i++;
-}
+
 }
 putchar('\n');
 return (0);
