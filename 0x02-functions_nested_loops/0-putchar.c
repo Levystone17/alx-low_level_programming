5155c52e45d6536939eac31889323713807d0c79
Levystone17 <tnlevy91@gmail.com>
2022-09-14 03:24:43 -0700

    task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void);
+{
+{
+int ch = "_putchar"
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
