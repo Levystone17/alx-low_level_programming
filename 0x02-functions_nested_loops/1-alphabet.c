ad0ed0ff74bc79ef95d7f3c9b8fa93d102df604f
Levystone17 <tnlevy91@gmail.com>
2022-09-14 05:25:56 -0700

    task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * main - check the code
+ *
+ * print_alphabet - prints alphabet
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+char c  = 'a';
+while (c <= 'z')
+putchar(c);
+a++;
+}
+
+int main(void)
+
+{
+print_alphabet();
+return (0);
+}
