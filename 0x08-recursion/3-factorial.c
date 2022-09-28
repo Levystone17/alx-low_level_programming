c109fcd716cad2f9da378d2f6f0cc6d47e0ad8a0
Levystone17 <tnlevy91@gmail.com>
2022-09-28 11:57:23 -0700

    task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * factorial - fonction that calculate factorial of given number
+ *
+ * @n: the given number to calculate
+ *
+ * Return: 0 for succes and -1 for an error
+ */
+
+int factorial(int n)
+
+{
+if (n <= 1)
+{
+return (1);
+}
+else
+
+return (n * factorial(n - 1));
+}
