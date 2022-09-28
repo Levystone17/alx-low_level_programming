cfe9df6fd4dc2f74acfdbcc75debf70a2ded0f98
Levystone17 <tnlevy91@gmail.com>
2022-09-28 08:25:47 -0700

    directory for recusion task

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: Togbe Nouza Levi
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x06-recusion directory.
+ */
+
+
+
+
+
+
+
+int _putchar(char c);
+int is_prime_number(int n);
+int _sqrt_recursion(int n);
+int _pow_recursion(int x, int y);
+int factorial(int n);
+int _strlen_recursion(char *s);
+void _print_rev_recursion(char *s);
+void _puts_recursion(char *s);
+
+#endif
