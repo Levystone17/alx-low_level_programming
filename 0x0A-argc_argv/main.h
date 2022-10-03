263114e1a496dc665a80e2f9221f2476979f0062
Levystone17 <tnlevy91@gmail.com>
2022-10-03 14:07:59 -0700

    task

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+/*
+ * File: main.h
+ * Auth: Togbe nouza levi
+ * Desc: Header file containing the prototypes of _putchar fonction
+ *
+ */
+
+int _putchar(char c);
+
+
+#endif
